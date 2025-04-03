c39a3454b51e53cd722e1437067d18797a139606
Lysanor <marcosalexandre.li@hotmail.com>
2025-04-03 19:50:19 -0300

    Revert "sprite do barco adicionado"
    This reverts commit 5f91042d75538679396b15d99d250d4ab2a9efc8.

--- titanic_game/jogo/objetos/titanic.py
+++ titanic_game/jogo/objetos/titanic.py
@@ -2,13 +2,12 @@ import pygame
 
 class Navio:
     def __init__(self, largura_janela, altura_janela):
-        self.sprite = pygame.image.load("titanic_game/sprites/barco/barco.png")
         self.largura = 60
         self.altura = 100
-        self.imagem = pygame.transform.scale(self.sprite, (self.largura, self.altura))
         self.x = largura_janela // 2 - self.largura // 2
         self.y = altura_janela - self.altura - 20
         self.velocidade = 5
+        self.cor = (200, 200, 200)
     
     def mover(self, direcao, limite_largura):
         if direcao == "esquerda" and self.x > 0:
@@ -17,7 +16,7 @@ class Navio:
             self.x += self.velocidade
     
     def desenhar(self, tela):
-         tela.blit(self.imagem, (self.x, self.y))
+        pygame.draw.rect(tela, self.cor, (self.x, self.y, self.largura, self.altura))
     
     def get_rect(self):
         return pygame.Rect(self.x, self.y, self.largura, self.altura)
