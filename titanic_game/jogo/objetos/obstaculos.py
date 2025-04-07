e8731544b17c77943d647e0b271340fb5809d944
Alice Santana <aliecsantana@gmail.com>
2025-04-07 14:35:22 -0300

    Atualizei o arquivo obstaculos.py

--- titanic_game/jogo/objetos/obstaculos.py
+++ titanic_game/jogo/objetos/obstaculos.py
@@ -2,31 +2,32 @@ import pygame
 import random
 
 class Obstaculo:
+    imagem = pygame.image.load("titanic_game/jogo/imagens/iceberg.png")
+    imagem = pygame.transform.scale(imagem, (50, 50))
+
     def __init__(self, posicao_x, posicao_y, velocidade):
         self.posicao_x = posicao_x
         self.posicao_y = posicao_y
         self.velocidade = velocidade
-        self.largura = random.randint(30, 50)
-        self.altura = random.randint(30, 50)
-        self.cor = (200, 230, 255)
+        self.largura = 50
+        self.altura = 50
         self.atingido = False
     
     def mover(self):
         self.posicao_y += self.velocidade
     
     def desenhar(self, tela):
-        pygame.draw.rect(tela, self.cor, (self.posicao_x, self.posicao_y, self.largura, self.altura))
-        pygame.draw.rect(tela, (255, 255, 255), (self.posicao_x + 5, self.posicao_y + 5, 10, 10))
+        tela.blit(Obstaculo.imagem, (self.posicao_x, self.posicao_y))
     
     def verificar_colisao(self, area_jogador):
         area_obstaculo = pygame.Rect(self.posicao_x, self.posicao_y, self.largura, self.altura)
         return area_obstaculo.colliderect(area_jogador)
 
 def criar_obstaculo_aleatorio(largura_tela):
-    posicao_x = random.randint(0, largura_tela - 60)
+    posicao_x = random.randint(0, largura_tela - 50)
     posicao_y = random.randint(-500, -50)
     velocidade = random.uniform(1.0, 3.0)
-    return Obstaculo(posicao_x, posicao_y, velocidade,)
+    return Obstaculo(posicao_x, posicao_y, velocidade)
 
 def criar_lista_obstaculos(largura_tela, quantidade):
-    return [criar_obstaculo_aleatorio(largura_tela) for _ in range(quantidade)]
+    return [criar_obstaculo_aleatorio(largura_tela) for i in range(quantidade)]
